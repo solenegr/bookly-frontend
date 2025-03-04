ad8a831e674be0cbb51a06266fb92a4c24c08a4d
Antony <antony64.rinaldo@gmail.com>
2025-03-04 17:13:50 +0100

    jesaispas

--- screens/BookDetailsScreen.jsx
+++ screens/BookDetailsScreen.jsx
@@ -11,7 +11,6 @@ import {
   Synopsis,
   Commentaires,
 } from "../components/book_details";
-import MaterialIcons from "@react-native-vector-icons/material-icons";
 
 import avis from "../data/avis.json";
 const BookDetailsScreen = () => {
@@ -36,12 +35,6 @@ const BookDetailsScreen = () => {
                 <Tome />
                 <Bookmark />
               </View>
-              <MaterialIcons
-                name="keyboard-backspace"
-                color="gray"
-                size={35}
-                onPress={() => navigation.goBack()}
-              />
               <Genres />
               <Synopsis />
               <Text className="text-gray-800 font-nunitoExtraBold text-xl mt-6">
